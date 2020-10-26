5200ea71d43f240c2eb759f605c95975615ccabf
Emily Giurleo <e.m.giurleo@gmail.com>
2020-10-26 17:49:08 -0400

    Properly use dotenv to load environment variables

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
+
+load_dotenv()
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
