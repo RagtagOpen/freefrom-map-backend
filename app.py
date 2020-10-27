0547b6fd9aa7a1986e012b70659b9697d17cb17e
Emily Giurleo <e.m.giurleo@gmail.com>
2020-10-27 16:39:30 -0400

    Create a category model and write tests proving that it works

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
-
 app = Flask(__name__)
 
 load_dotenv()
@@ -12,7 +11,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import State
+from models import Category 
 
 @app.route("/")
 def hello():
