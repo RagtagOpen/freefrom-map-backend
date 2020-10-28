35400fd3e6a8aa9498fe8d55523185bb52008a3f
Emily Giurleo <e.m.giurleo@gmail.com>
2020-10-28 17:30:08 -0400

    Add a new Criterion model (#22)

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Category 
+from models import Category, Criterion
 
 @app.route("/")
 def hello():
--- models.py
+++ models.py
@@ -20,3 +20,31 @@ class Category(db.Model):
             'title': self.title,
             'active': self.active,
         }
+
+class Criterion(db.Model):
+    __tablename__ = 'criteria'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False, index=True)
+    title = db.Column(db.String())
+    recommendation_text = db.Column(db.String())
+    active = db.Column(db.Boolean())
+
+    def __init__(self, category_id, title, recommendation_text, active):
+        self.category_id = category_id
+        self.title = title
+        self.recommendation_text = recommendation_text
+        self.active = active
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'category_id': self.category_id,
+            'title': self.title,
+            'recommendation_text': self.recommendation_text,
+            'active': self.active,
+        }
+
