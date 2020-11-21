d5c85c896e1aff58fe465e2acf7edeb34e7e567c
Danny Kim <danielkim1@protonmail.com>
2020-11-21 14:37:46 -0500

    Remove scoped auth

--- auth.py
+++ auth.py
@@ -82,18 +82,3 @@ def requires_auth(f):
         raise AuthError({"code": "invalid_header",
                          "description": "Unable to find appropriate key"}, 401)
     return decorated
-
-
-def requires_scope(required_scope):
-    """Determines if the required scope is present in the Access Token
-    Args:
-        required_scope (str): The scope required to access the resource
-    """
-    token = get_token_auth_header()
-    unverified_claims = jwt.get_unverified_claims(token)
-    if unverified_claims.get("scope"):
-        token_scopes = unverified_claims["scope"].split()
-        for token_scope in token_scopes:
-            if token_scope == required_scope:
-                return True
-    return False
