4e63d2cceb58ec42da3121d4f002ea585fb66c26
Emily Giurleo <e.m.giurleo@gmail.com>
2020-11-30 21:52:02 -0500

    Add test for authenticated endpoint

---
+++ tests/api/test_auth.py
@@ -0,0 +1,34 @@
+import unittest
+import json
+import requests
+import os
+
+from app import app, db
+from auth import AUTH0_DOMAIN, API_AUDIENCE
+
+class TestAuth(unittest.TestCase):
+  def setUp(self):
+    self.client = app.test_client()
+
+  def test_private_endpoint_no_auth(self):
+    response = self.client.get("/api/private")
+    self.assertEqual(response.status_code, 401)
+    json_response = json.loads(response.data.decode("utf-8"))
+
+    self.assertEqual(json_response["description"], "Authorization header is expected")
+  
+  def test_private_endpoint_auth(self):
+    data = {
+      "client_id": os.environ.get('AUTH0_CLIENT_ID'),
+      "client_secret": os.environ.get('AUTH0_CLIENT_SECRET'),
+      "audience": API_AUDIENCE,
+      "grant_type":"client_credentials"
+    }
+
+    jwt_response = requests.post(f'https://{AUTH0_DOMAIN}/oauth/token', data=data)
+    jwt = json.loads(jwt_response.text)['access_token']
+    
+    headers = {'Authorization': f'Bearer {jwt}'}
+
+    response = self.client.get("/api/private", headers=headers)
+    self.assertEqual(response.status_code, 200)
