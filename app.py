f7f2fb3b4eac7ed871b9ec7f8cd822eebbd4e258
Emily Giurleo <e.m.giurleo@gmail.com>
2020-12-06 20:02:08 -0500

    Add authenticated endpoint with tests (#52)
    Co-authored-by: Danny Kim <danielkim1@protonmail.com>

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
@@ -12,6 +13,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Category, Criterion, Link, Score
+from auth import AuthError, requires_auth
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+	response = jsonify(ex.error)
+	response.status_code = ex.status_code
+	return response
 
 @app.route("/categories")
 def get_categories():
@@ -72,6 +80,21 @@ def get_link(id_):
 		return  jsonify(link.serialize())
 	except Exception as e:
 		return(str(e))
+
+# This doesn't need authentication
+@app.route("/api/public")
+@cross_origin(headers=["Content-Type", "Authorization"])
+def public():
+    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+    return jsonify(message=response)
+
+# This needs authentication
+@app.route("/api/private")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@requires_auth
+def private():
+    response = "Hello from a private endpoint! You need to be authenticated to see this."
+    return jsonify(message=response)
 
 if __name__ == '__main__':
 		app.run()
