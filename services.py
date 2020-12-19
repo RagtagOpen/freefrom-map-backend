c6a4673c497b883fd4f6eeda208e84c49b76e0ba
Brittany Lau <brittanyplau@gmail.com>
2020-12-18 23:29:15 -0500

    POST/PUT Criteria endpoints (#65)
    * Mock token validation for non-auth tests
    
    * Clean up
    
    * POST and PUT criteria endpoints
    
    * Use services and patch auth
    
    * Remove require_auth0_secrets
    
    * Make category_id required
    
    * Create in services.py

--- services.py
+++ services.py
@@ -1,4 +1,4 @@
-from models import Category
+from models import Category, Criterion
 
 def update_or_create_category(data, category=Category()):
   """Takes a dict of data where the keys are fields of the category model.
@@ -16,3 +16,32 @@ def update_or_create_category(data, category=Category()):
     category.deactivate()
 
   return category
+
+
+def update_or_create_criterion(data, criterion=None):
+  """Takes a dict of data where the keys are fields of the criterion model.
+      Valid keys are category_id, title, recommendation_text, help_text, adverse,
+      and active. The 'active' key only uses a False value.
+  """
+
+  if criterion is None:
+    # TODO: Raise an appropriate error if category_id and state are not present
+    #  (see issue #57)
+    criterion = Criterion(category_id=data['category_id'])
+
+  if 'category_id' in data:
+    criterion.category_id = data['category_id']
+  if 'title' in data:
+    criterion.title = data['title']
+  if 'recommendation_text' in data:
+    criterion.recommendation_text = data['recommendation_text']
+  if 'help_text' in data:
+    criterion.help_text = data['help_text']
+  if 'adverse' in data:
+    criterion.adverse = data['adverse']
+
+  # You cannot reactivate a category after deactivating it
+  if 'active' in data and not data['active']:
+    criterion.deactivate()
+
+  return criterion
--- tests/api/test_criteria.py
+++ tests/api/test_criteria.py
@@ -1,9 +1,12 @@
 import unittest
+from unittest.mock import patch
 import json
+import datetime
 
 from app import app, db
 from models import Category, Criterion
-from tests.test_utils import clearDatabase, createCategory
+import strings
+from tests.test_utils import clearDatabase, createCategory, createCriterion, auth_headers
 
 class CriteriaTestCase(unittest.TestCase):
   def setUp(self):
@@ -100,9 +103,157 @@ class CriteriaTestCase(unittest.TestCase):
       "adverse": False
     })
 
-  def test_get_category_doesnt_exist(self):
+  def test_get_criterion_doesnt_exist(self):
     response = self.client.get("/criteria/1")
     self.assertEqual(response.status_code, 404)
     json_response = json.loads(response.data.decode("utf-8"))
 
     self.assertEqual(json_response["text"], "Criterion does not exist")
+
+  @patch('auth.is_token_valid', return_value=True)
+  def test_post_criterion(self, mock_auth):
+    category_id = self.category.id
+    data = {
+      "category_id": category_id,
+      "title": "Includes economic abuse framework",
+      "recommendation_text": "The state's definition of domestic violence should include a framework of economic abuse",
+      "help_text": "This means that the state acknowledges the role that economic control and abuse can play in domestic violence",
+      "adverse": False
+    }
+
+    response = self.client.post("/criteria", json=data, headers=auth_headers())
+    self.assertEqual(response.status_code, 201)
+    mock_auth.assert_called_once()
+
+    criterion = Criterion.query.one()
+    self.assertEqual(criterion.category_id, category_id)
+    self.assertEqual(criterion.title, "Includes economic abuse framework")
+    self.assertEqual(criterion.recommendation_text, "The state's definition of domestic violence should include a framework of economic abuse")
+    self.assertEqual(criterion.help_text, "This means that the state acknowledges the role that economic control and abuse can play in domestic violence")
+    self.assertFalse(criterion.adverse)
+    self.assertTrue(criterion.active)
+    self.assertIsNone(criterion.deactivated_at)
+
+    json_response = json.loads(response.data)
+    self.assertEqual(json_response, {
+      "id": criterion.id,
+      "category_id": category_id,
+      "title": "Includes economic abuse framework",
+      "recommendation_text": "The state's definition of domestic violence should include a framework of economic abuse",
+      "help_text": "This means that the state acknowledges the role that economic control and abuse can play in domestic violence",
+      "adverse": False,
+      "active": True,
+      "deactivated_at": None,
+    })
+
+  @patch('auth.is_token_valid', return_value=True)
+  def test_post_criterion_category_doesnt_exist(self, mock_auth):
+    category_id = self.category.id + 1
+    data = { "category_id": category_id }
+
+    response = self.client.post("/criteria", json=data, headers=auth_headers())
+    self.assertEqual(response.status_code, 404)
+    mock_auth.assert_called_once()
+
+    json_response = json.loads(response.data)
+    self.assertEqual(json_response['text'], strings.category_not_found)
+
+  def test_post_criterion_no_auth(self):
+    response = self.client.post("/criteria", data={}, headers={})
+    self.assertEqual(response.status_code, 401) 
+
+  @patch('auth.is_token_valid', return_value=True)
+  def test_put_criterion(self, mock_auth):
+    category_id = self.category.id
+    criterion = createCriterion(category_id)
+    db.session.add(criterion)
+    db.session.commit()
+
+    data = {
+      "title": "A New Title",
+      "help_text": "Some new help text",
+    }
+
+    response = self.client.put("/criteria/%i" % criterion.id, json=data, headers=auth_headers())
+    self.assertEqual(response.status_code, 200)
+    mock_auth.assert_called_once()
+
+    criterion = Criterion.query.first()
+    self.assertEqual(criterion.category_id, category_id)
+    self.assertEqual(criterion.title, "A New Title")
+    self.assertEqual(criterion.recommendation_text, "The state's definition of domestic violence should include a framework of economic abuse")
+    self.assertEqual(criterion.help_text, "Some new help text")
+    self.assertFalse(criterion.adverse)
+    self.assertTrue(criterion.active)
+    self.assertIsNone(criterion.deactivated_at)
+
+    json_response = json.loads(response.data)
+    self.assertEqual(json_response, {
+      "id": criterion.id,
+      "category_id": category_id,
+      "title": "A New Title",
+      "recommendation_text": "The state's definition of domestic violence should include a framework of economic abuse",
+      "help_text": "Some new help text",
+      "adverse": False,
+      "active": True,
+      "deactivated_at": None,
+    })
+
+  @patch('auth.is_token_valid', return_value=True)
+  def test_put_criterion_change_category(self, mock_auth):
+    category_id = self.category.id
+    criterion = createCriterion(category_id)
+    db.session.add(criterion)
+    db.session.commit()
+
+    data = { "category_id": category_id + 1 }
+
+    response = self.client.put("/criteria/%i" % criterion.id, json=data, headers=auth_headers())
+    self.assertEqual(response.status_code, 400)
+    mock_auth.assert_called_once()
+
+    json_response = json.loads(response.data)
+    self.assertEqual(json_response['text'], strings.criterion_cannot_change_category)
+
+  def test_put_category_no_auth(self):
+    response = self.client.put("/criteria/1", json={}, headers={})
+    self.assertEqual(response.status_code, 401)
+
+  @patch('auth.is_token_valid', return_value=True)
+  def test_put_criterion_doesnt_exist(self, mock_auth):
+    response = self.client.put("/criteria/1", json={}, headers=auth_headers())
+    self.assertEqual(response.status_code, 404)
+    mock_auth.assert_called_once()
+
+    json_response = json.loads(response.data)
+    self.assertEqual(json_response['text'], strings.criterion_not_found)
+
+  @patch('auth.is_token_valid', return_value=True)
+  def test_put_criterion_deactivate(self, mock_auth):
+    criterion = createCriterion(self.category.id)
+    db.session.add(criterion)
+    db.session.commit()
+
+    data = {
+      "active": False
+    }
+
+    response = self.client.put("/criteria/%i" % criterion.id, json=data, headers=auth_headers())
+    self.assertEqual(response.status_code, 200)
+    mock_auth.assert_called_once()
+
+    criterion = Criterion.query.first()
+
+    self.assertFalse(criterion.active)
+    self.assertTrue(isinstance(criterion.deactivated_at, datetime.datetime))
+
+    # Criterion cannot be reactivated
+    deactivated_at = criterion.deactivated_at
+    data = {
+      "active": True
+    }
+
+    response = self.client.put("/criteria/%i" % criterion.id, json=data, headers=auth_headers())
+    criterion = Criterion.query.first()
+    self.assertFalse(criterion.active)
+    self.assertEqual(criterion.deactivated_at, deactivated_at)
