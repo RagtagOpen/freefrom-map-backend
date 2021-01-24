e9b2defb535c155a756fce7d92e525b4c162cc01
Emily Giurleo <e.m.giurleo@gmail.com>
2021-01-24 11:53:04 -0500

    Import category data

--- data/imports.py
+++ data/imports.py
@@ -1,13 +1,38 @@
 import yaml
 import os
 
+from models import Category, Subcategory, Criterion
+
 SCRIPT_DIR = os.path.dirname(__file__)
 
+
 def absolute_file_path(file_name):
-  return os.path.join(SCRIPT_DIR, file_name)
+    return os.path.join(SCRIPT_DIR, file_name)
 
 
 def import_categories():
-  full_path = absolute_file_path('categories.yml')
-  with open(full_path) as file:
-    categories_info = yaml.load(file, Loader=yaml.FullLoader)
+    full_path = absolute_file_path('categories.yml')
+    with open(full_path) as file:
+        categories_data = yaml.load(file, Loader=yaml.FullLoader)
+        for category_data in categories_data:
+            category = Category(title=category_data['title']).save()
+
+            for subcategory_data in category_data['subcategories']:
+                subcategory = Subcategory(
+                    category_id=category.id,
+                    title=subcategory_data['title']
+                ).save()
+
+                criteria = []
+                for criterion_data in subcategory_data['ideal_criteria']:
+                    criteria.append(
+                        Criterion(subcategory_id=subcategory.id, title=criterion_data)
+                    )
+
+                for criterion_data in subcategory_data['adverse_criteria']:
+                    criteria.append(
+                        Criterion(subcategory_id=subcategory.id,
+                                  title=criterion_data, adverse=True)
+                    )
+
+                Criterion.save_all(criteria)
--- tests/data/test_import.py
+++ tests/data/test_import.py
@@ -1,8 +1,9 @@
 import unittest
 
-from app import app, db
+from app import db
 from tests.test_utils import clear_database
 from data.imports import import_categories
+from models import Category, Subcategory, Criterion
 
 
 class TestImport(unittest.TestCase):
@@ -10,4 +11,45 @@ class TestImport(unittest.TestCase):
         clear_database(db)
 
     def test_import_categories(self):
-      import_categories()
+        import_categories()
+
+        categories = Category.query.all()
+        self.assertEqual(len(categories), 9)
+        self.assertEqual(
+            list(map(lambda category: category.title, categories)),
+            [
+                'Economic Abuse Definition',
+                'Worker Protections',
+                'Civil Remedies and Protections',
+                'VOCA',
+                'Safety-Net Programs',
+                'Rental Protections',
+                'Coerced and Fraudulent Debt',
+                'Safe Banking Protections for Survivors',
+                'Reimagining Public Safety Responses to GBV'
+            ]
+        )
+
+        subcategories = Subcategory.query.all()
+        self.assertEqual(len(subcategories), 13)
+        self.assertEqual(
+            list(map(lambda subcategory: subcategory.title, subcategories)),
+            [
+                'Economic Abuse Definition',
+                'Paid and Protected Leave',
+                'Safe Work Environment',
+                'Unemployment Insurance',
+                'Designated Tort',
+                'Litigation Abuse',
+                'VOCA',
+                'SNAP',
+                'TANF',
+                'Rental Protections',
+                'Coerced and Fraudulent Debt',
+                'Safe Banking Protections for Survivors',
+                'Reimagining Public Safety Responses to GBV'
+            ]
+        )
+
+        criteria = Criterion.query.all()
+        self.assertEqual(len(criteria), 90)
